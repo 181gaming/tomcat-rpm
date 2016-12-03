e54e300868bfe59fd3725a55fabc802d7b15e047
Nicholas Houle <nicholashoule@users.noreply.github.com>
2016-12-02 22:58:17 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,5 @@ sudo ./vboxadd setup
 
 command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /home/vagrant/.rvm/scripts/rvm
 gem install fileutils
