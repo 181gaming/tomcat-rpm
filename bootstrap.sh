839b72d6f054442a4e096d32b93ce6afca026e09
Nicholas Houle <nicholashoule@users.noreply.github.com>
2016-12-02 22:46:10 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sudo yum update -y
 sudo yum install -y epel-release
-sudo yum install -y mock
+sudo yum install -y mock ruby rpm-build
 sudo usermod -a -G mock vagrant
 
 cd /opt/VBoxGuestAdditions-*/init
