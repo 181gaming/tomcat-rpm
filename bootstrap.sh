3b340a5d78c75c57124c920d6ca87c29220ce14b
Nicholas Houle <nicholashoule@users.noreply.github.com>
2016-12-02 22:49:18 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,6 @@ sudo usermod -a -G mock vagrant
 cd /opt/VBoxGuestAdditions-*/init
 sudo ./vboxadd setup
 
+command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 gem install fileutils
